7bb654bf0d99ac2a579fc72078d12032791ddcae
cgehuzi <mail.cgehuzi@gmail.com>
2024-07-31 22:37:51 +0200

    feat: add grid switcher to preview in guide

--- guide/components/Preview/index.js
+++ guide/components/Preview/index.js
@@ -5,9 +5,10 @@ import DefaultPreview from 'react-styleguidist/lib/client/rsg-components/Preview
 
 const Preview = ({ ...props }) => {
   const [background, setBackground] = React.useState('white');
+  const [hasGrid, setHasGrid] = React.useState(false);
 
   return (
-    <div className={classNames('guide-preview', `_${background}`)}>
+    <div className={classNames('guide-preview', `_${background}`, hasGrid && '_has-grid')}>
       <div className="guide-preview__switcher">
         <button
           className={classNames('guide-preview__switcher-button', {
@@ -26,6 +27,25 @@ const Preview = ({ ...props }) => {
           onClick={() => setBackground('black')}
         >
           Чёрный фон
+        </button>
+        <div className="guide-preview__switcher-separator" />
+        <button
+          className={classNames('guide-preview__switcher-button', {
+            _active: !hasGrid,
+          })}
+          type="button"
+          onClick={() => setHasGrid(false)}
+        >
+          Без сетки
+        </button>
+        <button
+          className={classNames('guide-preview__switcher-button', {
+            _active: hasGrid,
+          })}
+          type="button"
+          onClick={() => setHasGrid(true)}
+        >
+          С сеткой
         </button>
       </div>
       <DefaultPreview {...props} />
