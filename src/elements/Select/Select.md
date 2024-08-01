3f691c04c121a44fd27b170f559a721877df50b5
cgehuzi <mail.cgehuzi@gmail.com>
2024-08-01 21:29:10 +0200

    feat: add Select

---
+++ src/elements/Select/Select.md
@@ -0,0 +1,155 @@
+Базовый пример
+
+```js
+<Select value="price_asc">
+  <option value="default">По популярности</option>
+  <option value="price_asc">По возрастанию цены</option>
+  <option value="price_desc">По убыванию цены</option>
+</Select>
+```
+
+#### Плейсхолдер
+
+Селекту можно задать `placeholder`.
+
+```js
+<Select placeholder="Выберите сортировку">
+  <option value="">Не выбрана</option>
+  <option value="default">По популярности</option>
+  <option value="price_asc">По возрастанию цены</option>
+  <option value="price_desc">По убыванию цены</option>
+</Select>
+```
+
+#### Иконка
+
+Селекту можно задать feather-иконку. [Feather](https://feathericons.com/) – библиотека интерфейсных SVG-иконок.
+
+Иконка будет находиться слева от текста.
+
+```js
+<Select value="price_asc" feather="bar-chart">
+  <option value="">Не выбрана</option>
+  <option value="default">По популярности</option>
+  <option value="price_asc">По возрастанию цены</option>
+  <option value="price_desc">По убыванию цены</option>
+</Select>
+```
+
+#### Цвет
+
+Цвет компонента влияет на цвет обводки при фокусировке.
+
+```js
+import { Gapped } from '../../components/Gapped';
+
+const [color, setColor] = React.useState('');
+
+<Gapped isVertical>
+  <Select feather="pen-tool" placeholder="grey" color="grey" />
+  <Select feather="pen-tool" placeholder="first" color="first" />
+  <Select feather="pen-tool" placeholder="second" color="second" />
+  <Select feather="pen-tool" placeholder="third" color="third" />
+  <Select feather="pen-tool" placeholder="success" color="success" />
+  <Select feather="pen-tool" placeholder="error" color="error" />
+</Gapped>;
+```
+
+#### Размер
+
+```js
+import { Gapped } from '../../components/Gapped';
+
+<Gapped isVertical>
+  <Select placeholder="Размер micro" size="micro" />
+  <Select placeholder="Размер small" size="small" />
+  <Select placeholder="Размер base" />
+  <Select placeholder="Размер large" size="large" />
+  <Select placeholder="Размер extra" size="extra" />
+</Gapped>;
+```
+
+#### Состояния
+
+У селектов есть несколько состояний
+
+```js
+import { Gapped } from '../../components/Gapped';
+
+const [isWaiting, setIsWaiting] = React.useState(false);
+const [isInvalid, setIsInvalid] = React.useState(false);
+const [feather, setFeather] = React.useState('help-circle');
+
+const checkValue = (value) => {
+  if (!value) return;
+
+  // имитация асинхронного запроса
+  setIsWaiting(true);
+  setTimeout(() => {
+    if (value !== '6') {
+      setIsInvalid(true);
+      setFeather('x');
+    } else {
+      setIsInvalid(false);
+      setFeather('check');
+    }
+
+    setIsWaiting(false);
+  }, 3000);
+};
+
+<Gapped align="stretch" isVertical>
+  <Select disabled>
+    <option>в отключенном состоянии</option>
+  </Select>
+  <Select isInvalid>
+    <option>в состоянии ошибки</option>
+  </Select>
+  <Select isWaiting>
+    <option>в режиме ожидания</option>
+  </Select>
+  <Select
+    feather={feather}
+    placeholder="2 + 2 * 2"
+    isWaiting={isWaiting}
+    isInvalid={isInvalid}
+    onChange={(e) => {
+      const { value } = e.target;
+      setIsInvalid(false);
+      setFeather(value ? 'corner-down-left' : 'help-circle');
+      checkValue(value);
+      return value;
+    }}
+  >
+    <option value="">Не знаю</option>
+    <option>8</option>
+    <option>6</option>
+  </Select>
+</Gapped>;
+```
+
+#### Обработка событий
+
+Компонент поддерживает все стандарные обработчики нативного `input`. Но есть исключения...
+
+##### onChange
+
+Этот обработчик не только вызывает коллбэк, но и требует явно передать итоговое значение `value`.
+
+```js
+const removeJo = (value) => value.replaceAll('ё', 'е').replaceAll('Ё', 'Е');
+
+<Select
+  placeholder="Никаких букв ё!"
+  onChange={(e) => {
+    const { value } = e.target;
+    return removeJo(value);
+  }}
+>
+  <option value="">Ну ОК, е мое!</option>
+  <option>Ежик</option>
+  <option>Елка</option>
+  <option>Ёжик</option>
+  <option>Ёлка</option>
+</Select>;
+```
---
+++ src/elements/Select/Select.tsx
@@ -0,0 +1,148 @@
+import './styles/index.css';
+import React from 'react';
+import classNames from 'classnames';
+import FeatherIcon from 'feather-icons-react';
+
+export interface SelectProps extends React.HTMLAttributes<HTMLSelectElement> {
+  /** Значение `value` корневого `select` */
+  value: string;
+  /** Подсказка, показываемая при пустом `value` */
+  placeholder?: string;
+  /** Размер компонента */
+  size?: 'micro' | 'small' | 'base' | 'large' | 'extra';
+  /** Цвет компонента */
+  color?: 'grey' | 'first' | 'second' | 'third' | 'success' | 'error';
+  /** Название иконки Feather для отображения в левой части компонента */
+  feather?: string;
+  /** Отключает компонент */
+  disabled?: boolean;
+  /** Состояние ошибки */
+  isInvalid?: boolean;
+  /** Состояние ожидания */
+  isWaiting?: boolean;
+  /** Ссылка на HTML-элемент */
+  myRef?: React.RefObject<HTMLSelectElement>;
+  /** Обработчик изменения значения. Возвращаемое значение заменит `value` */
+  onChange?: (event: React.ChangeEvent<HTMLSelectElement>) => string | number;
+}
+
+/**
+ * Стилизованный `<select>`.
+ *
+ * Поддерживает все стандартные атрибуты и события элемента `select`.
+ */
+export class Select extends React.Component<SelectProps> {
+  myRef: React.RefObject<HTMLSelectElement>;
+
+  constructor(props: SelectProps) {
+    super(props);
+    this.myRef = props.myRef || React.createRef();
+  }
+
+  static defaultProps = {
+    size: 'base',
+    color: 'first',
+    isInvalid: false,
+    isWaiting: false,
+  };
+
+  state = {
+    value: this.props.value || '',
+  };
+
+  render() {
+    const {
+      size,
+      color,
+      feather,
+      isInvalid,
+      isWaiting,
+      value,
+      disabled,
+      placeholder,
+      className,
+      onChange,
+      ...props
+    } = this.props;
+
+    const childrenWithPlaceholder = React.Children.map(this.props.children, (child) => {
+      return child;
+    });
+
+    return (
+      <label
+        className={classNames(
+          'select',
+          isInvalid && 'select--invalid',
+          disabled && 'select--disabled',
+          isWaiting && 'select--waiting',
+          color && `select--${color}`,
+          size !== 'base' && `select--${size}`,
+          feather && 'select--with-icon',
+          className
+        )}
+      >
+        <select
+          className="select__field"
+          value={this.state.value}
+          disabled={disabled || isWaiting}
+          onChange={this.handleChange}
+          {...props}
+          ref={this.myRef}
+        />
+        {placeholder && (
+          <div
+            className={classNames(
+              'select__placeholder',
+              !this.state.value && 'select__placeholder--visible'
+            )}
+          >
+            {placeholder}
+          </div>
+        )}
+        {feather && (
+          <FeatherIcon
+            icon={isWaiting ? 'loader' : feather}
+            className={classNames('select__icon', isWaiting && 'select__icon--spinner')}
+          />
+        )}
+      </label>
+    );
+  }
+
+  handleChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    // если передан обработчик, то используем его
+    if (this.props.onChange) {
+      const nextValue = this.props.onChange(event);
+      // если обработчик передал значение
+      if (typeof nextValue === 'string' || typeof nextValue === 'number') {
+        // сохраняем изменение
+        this.setState({
+          value: String(nextValue),
+        });
+      }
+      // иначе отменяем изменение
+      return;
+    }
+
+    // если обработчик не передан, то используем состояние HTML-элемента
+    this.setState({
+      value: event.target.value,
+    });
+  };
+
+  componentDidMount(): void {
+    const value = this.myRef.current?.value || '';
+    this.setState({
+      value,
+    });
+  }
+
+  componentDidUpdate(prevProps: Readonly<SelectProps>): void {
+    if (prevProps.value !== this.props.value) {
+      this.setState({
+        value: this.props.value,
+      });
+    }
+  }
+}
